7f28398637be226270405103c2d79c3bb4ef0a61
Neal Terrell <neal.terrell@csulb.edu>
2021-03-01 09:51:01 -0800

    Lessons on first class and higher order functions.

--- FSharp/FirstClass/FirstClassFuncs.fs
+++ FSharp/FirstClass/FirstClassFuncs.fs
@@ -1,110 +1,61 @@
-﻿// It should not surprise you that functions are first class in F#.
+﻿// A first-class member can:
+// 1. Be assigned to a variable.
+
+let max a b = if a > b then a else b
+let remainder dividend divisor = dividend % divisor
 
 let f = max
-// f is of type int->int->int
-f 10 5 |> printfn "%d"
+// f is a *variable*. What is its type?
+
+// What can we do with a function variable? Call it!
+f 10 5 |> printfn "%d is the max"
 
 
-// square is of type int->int
-let square x = x * x
+// 2. Be passed as a parameter to a function.
+let useIntegers a b f =
+    if a < 0 then 
+        b + 1
+    else
+        f a b
 
-// isEven is of type int->bool
-let isEven n = 
-    n % 2 = 0
-
-// The List type has a function called "filter" which... you guessed it! Filters a list.
-
-let list1 = [1; 2; 3; 4; 5; 6; 7; 8; 9]
-
-printfn "%A" (List.filter isEven list1) // %O only prints first three elements; %A prints all
-// Or, with piping:
-list1 
-|> List.filter isEven 
-|> printfn "%A"
+// Perform type inference on this function and its parameters.
+// What type is.... a?
+//             .... b?
+//             .... f?
+//             .... useIntegers?
 
 
-// What is the TYPE of List.filter? Hint: combine what we know about generics and function types.
+// 3. Be returned from a function.
+let selectIntFunction a =
+    if a < 0 then
+        max
+    else
+        remainder
+
+// What type is selectIntFunction?
+
+// Putting it together...
+5
+|> selectIntFunction
+|> useIntegers 5 4
+|> printfn "result: %d"
 
 
+// 4. Be created dynamically at run-time.
+let input = System.Console.ReadLine() |> int
+let runtimeFunction =
+    if input > 0 then
+        // the "fun" keyword creates an anonymous function.
+        // it has no names, just arguments and a body.
+        // like all functions, it returns the last statement in the body.
+        (fun x y -> x + y)
+    else
+        (fun x y -> x - y)
 
+// What type is runtimeFunction?
+// Do we know *right now* which function body will be called?
+// But can we use it anyway? Yes!
 
-
-// Can we WRITE List.filter???
-
-
-
-
-
-// We also have List.map.
-list1
-|> List.filter isEven
-|> List.map square
-|> printfn "The even values, each squared: %A"
-
-
-// And fold, as List.reduce
-list1
-|> List.filter isEven
-|> List.map square
-|> List.reduce (+)
-|> printfn "The sum of those values is: %A"
-
-
-// Finally, how can functions be "dynamically created"? Two ways:
-// As an anonymous function with the "fun" keyword
-printfn "%A" (List.filter (fun x -> x % 2 = 1) list1)
-
-list1
-|> List.filter (fun x -> x % 2 = 1)
-|> List.map square
-|> printfn "The odd values, each squared: %A"
-
-
-
-
-// An example with strings
-let list2 = ["Neal"; "Mehrdad"; "Shannon"; "Josh"; "Anthony"]
-
-// Unfortunately the next line does not compile
-// printfn "%A" (List.filter (fun x -> x.Length > 4) list2)
-// WHy not?
-
-
-// Piping can help fix that
-list2 
-|> List.filter (fun x -> x.Length > 4)
-|> printfn "Names longer than 4 letters: %A"
-
-
-
-// As inner functions:
-let getAdder baseVal =
-    let adder x =
-        baseVal + x
-    adder
-
-// Examine the type of getAdder: int -> (int -> int)
-// getAdder returns a function that takes an int and returns an int...
-// but how is the return value of that function calculated? It uses baseVal's value
-// when getAdder is actually invoked. That is a DYNAMIC value that is not known until
-// runtime, so the behavior of the resulting function won't be known until runtime. 
-
-
-// To use it:
-let addBy5 = getAdder 5
-addBy5 10 |> printfn "5 + 10 = %d"
-
-// Calling getAdder multiple times will result in multiple copies of "adder" being
-// created, each with a different value of "baseVal".
-let addBy100 = getAdder 100
-addBy100 10 |> printfn "100 + 10 = %d"
-
-
-// This may be troubling... how does it POSSIBLY work?
-
-
-
-
-
-
-
+runtimeFunction
+|> useIntegers -5 10
+|> printfn "runtime result: %d"
---
+++ FSharp/HigherOrder/HigherOrder.fs
@@ -0,0 +1,96 @@
+﻿open System
+
+// A "higher order function" is one that uses another function as one of its parameters.
+// What's the use? Like all functions, it's about abstracting patterns to make code more robust and reusable.
+
+// Find the first even integer in a list.
+let rec findFirstEven coll =
+    match coll with 
+    | []                    -> failwith "can't find an integer from an empty list"
+    | h :: _ when h % 2 = 0 -> h
+    | _ :: t                -> findFirstEven t
+
+
+[1; 3; 5; 6; 7; 8]
+|> findFirstEven
+|> printfn "first even: %d"
+
+
+// How about... findFirstOdd?
+let rec findFirstOdd coll =
+    match coll with 
+    | []                    -> failwith "can't find an integer from an empty list"
+    | h :: _ when h % 2 = 1 -> h
+    | _ :: t                -> findFirstOdd t
+
+
+// How about... findFirstVowel?
+let rec findFirstVowel coll =
+    match coll with 
+    | []                    -> failwith "can't find a character from an empty list"
+    | h :: _ when h = 'a' || h = 'e' || h = 'i' || h = 'o' || h = 'u' -> h
+    | _ :: t                -> findFirstVowel t
+
+
+// What can we abstract about these functions?
+// They each iterate through a list of SOME TYPE; test the values of that type under a CONDITION; 
+// and return a value of that type.
+// A CONDITION is really just a FUNCTION that returns boolean (a "predicate").
+let rec findFirst pred coll =
+    match coll with
+    | []                 -> failwith "can't find the first in an empty list"
+    | h :: _ when pred h -> h
+    | _ :: t             -> findFirst pred t
+
+// What is the type of findFirst?
+// Your first higher-order function!
+
+
+["Neal"; "Anthony"; "Mehrdad"; "Claus"; "Jaclyn"]
+|> findFirst (fun s -> String.length s > 5)
+|> printfn "First long name: %s"
+
+// Now, ANY time we have a list of values and want to know the first value that satisfies some condition...
+// we can use findFirst!
+// (Really we would use List.find, which is this function built into F#)
+[("Victor", 3.3); ("Ada", 3.8); ("Barry", 2.9)]
+|> List.find (fun (n, gpa) -> gpa >= 3.5) // (n, gpa) means the param is a tuple with 2 values
+|> printfn "First dean's list student: %O"
+
+
+
+// Now we study 3 very important and famous higher-order functions; the foundation of functional programming.
+
+// 1. Filter: given a predicate and a list of values, return a new list of only those values 
+// that pass the predicate.
+["Neal"; "Anthony"; "Mehrdad"; "Claus"; "Jaclyn"]
+|> List.filter (fun s -> String.length s > 5)
+|> printfn "All long names: %O"
+
+// What type is List.filter?
+
+// Can we write it?
+let rec filter pred coll =
+    match coll with 
+    | []                 -> []
+    | h :: t when pred h -> h :: filter pred t
+    | _ :: t             -> filter pred t
+
+["Neal"; "Anthony"; "Mehrdad"; "Claus"; "Jaclyn"]
+|> filter (fun s -> String.length s > 5)
+|> printfn "My long names: %O"
+
+// Can we write it with TAIL recursion? 
+let filterTail pred coll =
+    let rec filterTailImpl pred coll acc = // acc = the result of filtering the items before this point.
+        match coll with
+        | []                 -> acc
+        // problem: how to combine h with acc?
+        | h :: t when pred h -> filterTailImpl pred t (h :: acc) // uh oh... look at the result of this...
+        | _ :: t             -> filterTailImpl pred t acc
+
+    filterTailImpl pred coll []
+
+["Neal"; "Anthony"; "Mehrdad"; "Claus"; "Jaclyn"]
+|> filterTail (fun s -> String.length s > 5)
+|> printfn "All long names???: %O"
